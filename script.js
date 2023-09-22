466c4f09264a518cdf0230123e355d17a03c15d6
Lindsay Walker <lawalker98@gmail.com>
2023-09-22 14:26:07 -0400

    daily update

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('searchButton')
+
+console.log("working")
+
+
+
+ button.addEventListener('click', async()  => {
+    
+    let memoryInput  = textInput.value
+    let  response = await axios.get('https://api.pexels.com/v1/')
+console.log(response)
+const client = createClient('rF21hoFszR2gtJzzi24lyS6YsZEqsYpBlj3gWr5ASNF8f8e2Oeiey92H')
+
+const query = 'animals'
+})
+//import { createClient } from 'pexels'
