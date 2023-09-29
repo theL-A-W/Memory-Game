16c751ffbb4d00fddbd32c22031968759e5159ef
Lindsay Walker <lawalker98@gmail.com>
2023-09-29 07:46:45 -0400

    final changes

--- script.js
+++ script.js
@@ -2,13 +2,36 @@
 //Dad jokes API//
 
 const jokeButton = document.querySelector('.dadJoke')
+let matchedCount = 0
 
 jokeButton.addEventListener('click', getJoke)
+const imageArray = [
+    `https://media.istockphoto.com/id/1392163011/photo/cute-female-kitten-walking-towards-and-looking-in-the-camera-on-a-pink-background.webp?b=1&s=170667a&w=0&k=20&c=YA2ZIsdH9Qpah-nmbhOvsz9DP8pLJIJ19FUaYAPolM0=`,
+    `https://media.istockphoto.com/id/1392163011/photo/cute-female-kitten-walking-towards-and-looking-in-the-camera-on-a-pink-background.webp?b=1&s=170667a&w=0&k=20&c=YA2ZIsdH9Qpah-nmbhOvsz9DP8pLJIJ19FUaYAPolM0=`,
+    `https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHB1cHB5fGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHB1cHB5fGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
+    `https://media.istockphoto.com/id/184154297/photo/small-yellow-chickens-on-a-white-background.webp?b=1&s=170667a&w=0&k=20&c=c_BLTOZ00XZ_XSK5SABhcUKmZ0lEimw8-rX7OsX-S8M=`,
+    `https://media.istockphoto.com/id/184154297/photo/small-yellow-chickens-on-a-white-background.webp?b=1&s=170667a&w=0&k=20&c=c_BLTOZ00XZ_XSK5SABhcUKmZ0lEimw8-rX7OsX-S8M=`,
+    `https://images.unsplash.com/photo-1515536765-9b2a70c4b333?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cG9ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1515536765-9b2a70c4b333?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cG9ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://media.istockphoto.com/id/1330878548/photo/cute-calf-with-red-and-white-coat-large-big-eyes-and-pink-nose-lovely-and-innocent.webp?b=1&s=170667a&w=0&k=20&c=NkOzLjkV6ntL9aiFPLSSRWx9n5F5ku3IPCEtTLgUPWs=`,
+    `https://media.istockphoto.com/id/1330878548/photo/cute-calf-with-red-and-white-coat-large-big-eyes-and-pink-nose-lovely-and-innocent.webp?b=1&s=170667a&w=0&k=20&c=NkOzLjkV6ntL9aiFPLSSRWx9n5F5ku3IPCEtTLgUPWs=`,
+    `https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8emVicmF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`, 
+    `https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8emVicmF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1548506446-86bfddac5620?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdpcmFmZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1548506446-86bfddac5620?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdpcmFmZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://media.istockphoto.com/id/165847424/photo/goat.webp?b=1&s=170667a&w=0&k=20&c=AJqrrmwgsCp03Iq5upV5hIpQv3krJjpF9ygeRcMgwp0=`,
+    `https://media.istockphoto.com/id/165847424/photo/goat.webp?b=1&s=170667a&w=0&k=20&c=AJqrrmwgsCp03Iq5upV5hIpQv3krJjpF9ygeRcMgwp0=`,
+    `https://images.unsplash.com/photo-1602847213180-50e43a80bef4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNoZWVwfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1602847213180-50e43a80bef4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNoZWVwfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1579822269843-3769559ce9bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZHVja2xpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1579822269843-3769559ce9bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZHVja2xpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1555010133-d883506aedee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3F1aXJyZWwlMjBmdW5ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://images.unsplash.com/photo-1555010133-d883506aedee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3F1aXJyZWwlMjBmdW5ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
+    `https://media.istockphoto.com/id/162136091/photo/kangaroo-posing-very-much-like-a-human.webp?b=1&s=170667a&w=0&k=20&c=UXGdXCFcNXlWhHppK6gxNvZC6oPehfrUn4CZrpUyywg=`,
+    `https://media.istockphoto.com/id/162136091/photo/kangaroo-posing-very-much-like-a-human.webp?b=1&s=170667a&w=0&k=20&c=UXGdXCFcNXlWhHppK6gxNvZC6oPehfrUn4CZrpUyywg=`
+]
 
-async function displayJoke(setup, punchline){
-  jokeButton.innerHTML = `<h5><strong>Joke: </strong></h5><p>${setup}</p><h5><strong>Punchline: </strong></h5><p>${punchline}</p>`
-
-}
 async function getJoke(){
     try {
         const options = {
@@ -27,39 +50,13 @@ async function getJoke(){
           
 
     }              
+    async function displayJoke(setup, punchline){
+        jokeButton.innerHTML = `<h5><strong>Joke: </strong></h5><p>${setup}</p><h5><strong>Punchline: </strong></h5><p>${punchline}</p>`
+      
+      }
 
-     
 
 
-
-let matchedCount = 0
-
-    const imageArray = [
-        `https://media.istockphoto.com/id/1392163011/photo/cute-female-kitten-walking-towards-and-looking-in-the-camera-on-a-pink-background.webp?b=1&s=170667a&w=0&k=20&c=YA2ZIsdH9Qpah-nmbhOvsz9DP8pLJIJ19FUaYAPolM0=`,
-        `https://media.istockphoto.com/id/1392163011/photo/cute-female-kitten-walking-towards-and-looking-in-the-camera-on-a-pink-background.webp?b=1&s=170667a&w=0&k=20&c=YA2ZIsdH9Qpah-nmbhOvsz9DP8pLJIJ19FUaYAPolM0=`,
-        `https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHB1cHB5fGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHB1cHB5fGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
-        `https://media.istockphoto.com/id/184154297/photo/small-yellow-chickens-on-a-white-background.webp?b=1&s=170667a&w=0&k=20&c=c_BLTOZ00XZ_XSK5SABhcUKmZ0lEimw8-rX7OsX-S8M=`,
-        `https://media.istockphoto.com/id/184154297/photo/small-yellow-chickens-on-a-white-background.webp?b=1&s=170667a&w=0&k=20&c=c_BLTOZ00XZ_XSK5SABhcUKmZ0lEimw8-rX7OsX-S8M=`,
-        `https://images.unsplash.com/photo-1515536765-9b2a70c4b333?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cG9ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1515536765-9b2a70c4b333?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cG9ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://media.istockphoto.com/id/1330878548/photo/cute-calf-with-red-and-white-coat-large-big-eyes-and-pink-nose-lovely-and-innocent.webp?b=1&s=170667a&w=0&k=20&c=NkOzLjkV6ntL9aiFPLSSRWx9n5F5ku3IPCEtTLgUPWs=`,
-        `https://media.istockphoto.com/id/1330878548/photo/cute-calf-with-red-and-white-coat-large-big-eyes-and-pink-nose-lovely-and-innocent.webp?b=1&s=170667a&w=0&k=20&c=NkOzLjkV6ntL9aiFPLSSRWx9n5F5ku3IPCEtTLgUPWs=`,
-        `https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8emVicmF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`, 
-        `https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8emVicmF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1548506446-86bfddac5620?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdpcmFmZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1548506446-86bfddac5620?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdpcmFmZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://media.istockphoto.com/id/165847424/photo/goat.webp?b=1&s=170667a&w=0&k=20&c=AJqrrmwgsCp03Iq5upV5hIpQv3krJjpF9ygeRcMgwp0=`,
-        `https://media.istockphoto.com/id/165847424/photo/goat.webp?b=1&s=170667a&w=0&k=20&c=AJqrrmwgsCp03Iq5upV5hIpQv3krJjpF9ygeRcMgwp0=`,
-        `https://images.unsplash.com/photo-1602847213180-50e43a80bef4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNoZWVwfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1602847213180-50e43a80bef4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNoZWVwfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1579822269843-3769559ce9bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZHVja2xpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1579822269843-3769559ce9bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZHVja2xpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1555010133-d883506aedee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3F1aXJyZWwlMjBmdW5ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://images.unsplash.com/photo-1555010133-d883506aedee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3F1aXJyZWwlMjBmdW5ueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60`,
-        `https://media.istockphoto.com/id/162136091/photo/kangaroo-posing-very-much-like-a-human.webp?b=1&s=170667a&w=0&k=20&c=UXGdXCFcNXlWhHppK6gxNvZC6oPehfrUn4CZrpUyywg=`,
-        `https://media.istockphoto.com/id/162136091/photo/kangaroo-posing-very-much-like-a-human.webp?b=1&s=170667a&w=0&k=20&c=UXGdXCFcNXlWhHppK6gxNvZC6oPehfrUn4CZrpUyywg=`
-    ]
 const cards = document.querySelectorAll('.card')
 let isFlipped = Array(cards.length).fill(false)
 let firstCard, secondCard
@@ -105,6 +102,7 @@ function flipCard(index) {
     if (firstCardURL === secondCardURL){
         matchedCount += 2
         resetCards()
+
 // TO DISPLAY POP-UP number of matches is equal to the length of the  array ///////
     if (matchedCount === imageArray.length) {
     winningPopup()
@@ -145,10 +143,6 @@ function flipCard(index) {
     shuffleArray(imageArray)
 
 
-
-
-
-
 //Timer functionality
     function startTimer(duration, display) {
         let timer = duration, minutes, seconds
@@ -182,7 +176,6 @@ if (timer <= 0 && matchedCount < imageArray.length){
         
     }
 
-   
 
 //this starts a 1:00 minute timer when the "hard" button is pressed
     hardButton = document.getElementById('hard')
